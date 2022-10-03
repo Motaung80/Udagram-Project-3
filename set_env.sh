c88b6595f62fcb6c4075bb39d73300b27b2321bc
Motaung80 <motaung80.com>
2022-10-03 21:50:22 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Xz6.+MM+n!p%i?C
+export POSTGRES_HOST=db-motaung.cj8k6ghz1jxf.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-577567478331-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
