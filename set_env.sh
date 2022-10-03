35b8867604fd555174f75da7ce921f7b26c53229
Motaung80 <motaung80.com>
2022-10-03 22:50:11 +0200

    1 commit

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=Xz6.+MM+n!p%i?C
-export POSTGRES_HOST=db-motaung.cj8k6ghz1jxf.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=---------------------------------------
+export POSTGRES_HOST=----------------------
 export POSTGRES_DB=postgres
-export AWS_BUCKET=udagram-577567478331-dev
+export AWS_BUCKET=--------------------------
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
